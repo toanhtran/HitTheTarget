b7d8e62e1f4a14c54ee423cad7011ec567fbc862
toanhtran <toanh.t.tran@gmail.com>
2018-01-08 13:53:12 -0800

    aboutview controller for i btn

---
+++ HitTheTarget/AboutViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AboutViewController.swift
+//  HitTheTarget
+//
+//  Created by ToAnh T. Tran on 1/8/18.
+//  Copyright © 2018 ToAnh T. Tran. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+	
+	@IBAction func close() {
+		dismiss(animated: true, completion: nil)
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+}
